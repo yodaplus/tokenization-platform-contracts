0982b038387c634a339b2f6a6e528adee302cab2
Nitish <nitishp74@gmail.com>
2021-11-19 11:14:16 +0530

    removed pragma

--- contracts/CustodianContract.sol
+++ contracts/CustodianContract.sol
@@ -1,6 +1,5 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
-pragma experimental ABIEncoderV2;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import {Token as TokenContract} from "./Token.sol";
 import "./ICustodianContract.sol";
